f72851c7c09924f7e0004750e0169d8203c34d78
FanoSina <messinastefano14@hotmail.com>
2025-12-16 21:08:07 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 /**************************************************
  * SUPABASE CONFIG
  **************************************************/
-const SUPABASE_URL = 'https://gpnmrtbwyytybdblmtka.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imdwbm1ydGJ3eXl0eWJkYmxtdGthIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjU5MDIwNTksImV4cCI6MjA4MTQ3ODA1OX0.pGhuP0P0fm8xFmMUy707_i0TKVdE1P_9-bOq9DNWZfI';
-
-const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+const supabaseUrl = 'https://gpnmrtbwyytybdblmtka.supabase.co'
+const supabaseKey = process.env.eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imdwbm1ydGJ3eXl0eWJkYmxtdGthIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjU5MDIwNTksImV4cCI6MjA4MTQ3ODA1OX0.pGhuP0P0fm8xFmMUy707_i0TKVdE1P_9-bOq9DNWZfI
+const supabase = createClient(supabaseUrl, supabaseKey)
 
 const $ = (id) => document.getElementById(id);
 let currentUser = null;
